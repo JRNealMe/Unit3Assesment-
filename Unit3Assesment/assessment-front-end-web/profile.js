1a736838ae863a0219795f8356f497da47b65342
Jared Neal <99784946+JRNealMe@users.noreply.github.com>
2022-04-01 16:38:23 -0600

    Add files via upload

---
+++ Unit3Assesment/assessment-front-end-web/profile.js
@@ -0,0 +1,13 @@
+const color = document.getElementById('color')
+const place = document.getElementById('place')
+const ritual = document.getElementById('ritual')
+
+color.addEventListener('click', (e)=>{
+    alert('green')
+})
+place.addEventListener('click', (e)=>{
+    alert('home')
+})
+ritual.addEventListener('click', (e)=>{
+    alert('be with kids')
+})
